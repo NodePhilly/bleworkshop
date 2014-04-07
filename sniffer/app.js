e5b2e92b7b4238c33621273010c7719926486615
bryan paluch <bryanpaluch@gmail.com>
2014-04-07 19:51:40 -0400

    added filter stream

--- sniffer/app.js
+++ sniffer/app.js
@@ -1,9 +1,11 @@
 var sniffer = require('./lib/sniffer');
-var processor = require('./lib/processor');
+var filter = require('./lib/filter');
+var processor = require('./lib/processor'); 
 var notifier = require('./lib/notifier');
 
 
 //sniffer create a readStream
 sniffer
+.pipe(filter)
 .pipe(processor) // Processor is a through stream
 .pipe(notifier); // notifier is a through stream as well
---
+++ sniffer/lib/filter.js
@@ -0,0 +1,26 @@
+var es = require("event-stream");
+
+// Filter from ENV or Bryans fitbit
+var filtered = process.env.UUID || 'f44e22f2ef25';
+
+module.exports = es.through(function write(data){
+  /* example data
+   *
+   * { uuid: 'f44e22f2ef25',
+   *   advertisement: 
+   *     { localName: 'Force',
+   *       txPowerLevel: -6,
+   *       manufacturerData: undefined,
+   *       serviceData: [ [Object] ],
+   *       serviceUuids: [ 'adab19396e7d4601bda2bffaa68956ba' ] },
+   *   rssi: -61,
+   *   services: null,
+   *   state: 'disconnected' }
+   */
+  
+  if(data.uuid !== filtered) return;
+
+  this.emit("data", data);
+
+    
+});
